62c7a84efacbc46fa5db0e8c05d7f72d293c9e2e
Tom Benzaia <tbenzaia@gmail.com>
2015-03-07 13:04:32 -0600

    added date

--- js/retain.js
+++ js/retain.js
@@ -20,7 +20,8 @@ $(function(){
     var octopus = {
         addNewNote: function(noteStr) {
             model.add({
-                content: noteStr
+                content: noteStr,
+                date: new Date()
             });
             view.render();
         },
@@ -53,6 +54,9 @@ $(function(){
             octopus.getNotes().forEach(function(note){
                 htmlStr += '<li class="note">'+
                         note.content +
+                        '<span class="note-date">' +
+                        	new Date(note.date).toString() +
+                        '</span>' +
                     '</li>';
             });
             this.noteList.html( htmlStr );
